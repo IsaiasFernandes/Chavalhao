9107108606006647f83705c2b7fb0fafb1dc13f9
IsaiasFernandes <isaiasfernandes1928@hotmail.com>
2020-03-19 19:29:09 -0300

    Projeto React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Global from "./styles/global";
+
+import Main from "./pages/Main/";
+
+const App = () => (
+  <>
+    <Global />
+    <Main />
+  </>
+);
+
+export default App;
